50ea697e149aa28b70f63abb52c656b1d000b9f0
Tiru-99 <aayushtirmanwar1234@gmail.com>
2026-01-19 22:57:12 +0530

    add : some more changes

--- src/get_searches.cpp
+++ src/get_searches.cpp
@@ -14,7 +14,7 @@ int main() {
     auto parsed_input = parse_input(input);
     auto scored_docs  = score_all_documents(parsed_input);
 
-    // -------- print results --------
+
     std::cout << "\nSearch results:\n";
 
     for (const auto& [docId, score] : scored_docs) {
